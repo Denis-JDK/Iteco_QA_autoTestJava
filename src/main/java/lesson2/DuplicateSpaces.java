4b83047852ff96655dbeb992561d0361ec1152a8
Shapoval Denis <speleo04@yandex.ru>
2023-10-20 13:53:31 +0300

    add duplicate spaces class

---
+++ src/main/java/lesson2/DuplicateSpaces.java
@@ -0,0 +1,12 @@
+package lesson2;
+
+public class DuplicateSpaces {
+
+    public static void main(String[] args) {
+
+        String yourString = "I have lern  Java good         work  and  company";
+       String result =yourString.replaceAll("\\s+", " ");
+
+        System.out.println(result);
+    }
+}
