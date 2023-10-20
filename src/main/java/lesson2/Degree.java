7410dde9565f693c7b9007663ba689959e88d3cc
Shapoval Denis <speleo04@yandex.ru>
2023-10-20 13:39:24 +0300

    add class degree

---
+++ src/main/java/lesson2/Degree.java
@@ -0,0 +1,15 @@
+package lesson2;
+
+public class Degree {
+    public static void main(String[] args) {
+        int count = 9;
+
+        if (count%2 == 0) {
+            double result = Math.pow(count, 10);
+            System.out.println(result);
+        } else {
+            double result = Math.pow(count, 3);
+            System.out.println(result);
+        }
+    }
+}
